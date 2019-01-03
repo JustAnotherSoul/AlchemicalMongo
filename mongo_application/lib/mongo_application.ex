fbe622a32640c503cdde9b336e7a1f0057adabe7
Louis <cbasile1391@gmail.com>
2019-01-02 20:48:15 -0600

    Open connection, create, and find records in MongoDB

--- mongo_application/lib/mongo_application.ex
+++ mongo_application/lib/mongo_application.ex
@@ -4,15 +4,24 @@ defmodule MongoApplication do
   """
 
   @doc """
-  Hello world.
+  Start a mongo connection, and query for the given string
 
   ## Examples
 
       iex> MongoApplication.hello()
-      :world
 
   """
-  def hello do
-    :world
+  def open_connection do
+    {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:27017/test")
+    conn
+  end
+
+  def insert_record(conn, record) do
+    Mongo.insert_one(conn, "lines", record)
+  end
+
+  def get_record(conn, record) do
+    cursor = Mongo.find(conn, "lines", %{"$and" => [record]})
+    cursor |> Enum.to_list() 
   end
 end
